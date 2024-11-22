6607722d76fb7b4ddd5cd0d9968c63190d4102ff
Leon Chen <lrchen01@gmail.com>
2024-11-22 03:10:26 -0500

    fix: remove next-video remnants

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
-const { withNextVideo } = require('next-video/process')
-
 module.exports = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: [`@svgr/webpack`],
-    });
-
-    config.module.rules.push({
-      test: /\.mp4$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[hash].[ext]',
-          outputPath: 'videos/',
-        },
-      },
     });
 
     return config;
