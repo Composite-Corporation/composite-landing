6463fd38ba17d23c0a3a3e79e48e543a6c2bf775
Leon Chen <lrchen01@gmail.com>
2024-11-22 03:04:46 -0500

    fix: removed react-particles library

--- src/pages/case-studies.tsx
+++ src/pages/case-studies.tsx
@@ -1,34 +1,34 @@
 import { tw } from 'twind';
-import Particles from 'react-particles-js';
+// import Particles from 'react-particles-js';
 import Page from '@/components/page';
 
-const ParticleBg = () => (
-  <Particles
-    params={{
-      particles: {
-        number: {
-          value: 400,
-          density: { enable: true, value_area: 3000 },
-        },
-        line_linked: { enable: false },
-        move: { direction: `right`, speed: 0.3 },
-        size: { value: 1 },
-        opacity: { anim: { enable: true, speed: 0.5, opacity_min: 0.1 } },
-      },
-      interactivity: { events: { onclick: { enable: false } } },
-      retina_detect: true,
-    }}
-  />
-);
+// const ParticleBg = () => (
+//   <Particles
+//     params={{
+//       particles: {
+//         number: {
+//           value: 400,
+//           density: { enable: true, value_area: 3000 },
+//         },
+//         line_linked: { enable: false },
+//         move: { direction: `right`, speed: 0.3 },
+//         size: { value: 1 },
+//         opacity: { anim: { enable: true, speed: 0.5, opacity_min: 0.1 } },
+//       },
+//       interactivity: { events: { onclick: { enable: false } } },
+//       retina_detect: true,
+//     }}
+//   />
+// );
 
 const CaseStudy = () => (
   <div>
     {/* Hero Section */}
     <section>
       <div className={tw(`w-full min-h-screen bg-gray-900 relative`)}>
-        <div className={tw(`absolute left-0 top-0 h-screen w-full overflow-hidden`)}>
+        {/* <div className={tw(`absolute left-0 top-0 h-screen w-full overflow-hidden`)}>
           <ParticleBg />
-        </div>
+        </div> */}
         <div className={tw(`max-w-7xl mx-4 lg:mx-auto pt-8 lg:pt-16`)}>
           <h1 className={tw(`text-white text-4xl lg:text-7xl font-bold text-center`)}>
             Proven Success with Scientific Laboratory Supplies (SLS)
@@ -41,7 +41,7 @@ const CaseStudy = () => (
             <img
               src={`/images/sls.png`}
               alt={`Case Study Image`}
-              className={tw(`py-12 object-cover overflow-hidden rounded-lg`)} // Adjusted spacing
+              className={tw(`py-12 object-cover overflow-hidden rounded-lg`)}
               width={400}
               height={200}
             />
